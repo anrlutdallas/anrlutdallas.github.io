ca59274c537bb6b2696fec6d78d841c513a05e07
Caleb Fung <caleb.t.fung@gmail.com>
2017-05-17 21:30:08 -0500

    update pages

---
+++ assets/js/footer.js
@@ -0,0 +1,17 @@
+document.write('\
+<!-- start footer -->\
+<div id="footer">\
+  <div class="container">\
+\
+    <!-- copyright -->\
+    <div class="copyright">\
+      <ul class="menu">\
+        <li>&copy; 2017 Advanced Networks Research Lab.</li>\
+        <li><a href="http://www.utdallas.edu/" target="_blank">The University of Texas at Dallas.</a></li>\
+      </ul>\
+    </div>\
+\
+  <!-- end footer -->\
+  </div>\
+</div>\
+');
