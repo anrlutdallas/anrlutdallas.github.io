a9fb220c49db11a85ff0b956b181b6c5b2d43147
Caleb Fung <caleb.t.fung@gmail.com>
2017-05-17 21:59:08 -0500

    fix links

--- assets/js/header.js
+++ assets/js/header.js
@@ -14,40 +14,40 @@ document.write('\
   <!-- start nav -->\
   <nav id="nav">\
     <ul>\
-      <li><a href="index.html">Home</a></li>\
+      <li><a href="/index.html">Home</a></li>\
       <li>\
-        <a href="team.html">Team</a>\
+        <a href="/team.html">Team</a>\
         <ul>\
-          <li><a href="team.html#faculty">Faculty</a>\
+          <li><a href="/team.html#faculty">Faculty</a>\
             <ul>\
-              <li><a href="team.html#jason-p-jue">Dr. Jason P. Jue</a></li>\
+              <li><a href="/team.html#jason-p-jue">Dr. Jason P. Jue</a></li>\
             </ul>\
           </li>\
-          <li><a href="team.html#visiting-scholars">Visiting Scholars</a></li>\
-          <li><a href="team.html#students">Students</a>\
+          <li><a href="/team.html#visiting-scholars">Visiting Scholars</a></li>\
+          <li><a href="/team.html#students">Students</a>\
             <ul>\
-              <li><a href="team.html?id=undergraduate#undergraduate">Undergraduate</a></li>\
-              <li><a href="team.html?id=graduate#graduate">Graduate</a></li>\
-              <li><a href="team.html?id=alumni#alumni">Alumni</a></li>\
+              <li><a href="/team.html?id=undergraduate#undergraduate">Undergraduate</a></li>\
+              <li><a href="/team.html?id=graduate#graduate">Graduate</a></li>\
+              <li><a href="/team.html?id=alumni#alumni">Alumni</a></li>\
             </ul>\
           </li>\
         </ul>\
       </li>\
-      <li><a href="publications.html">Publications</a>\
+      <li><a href="/publications.html">Publications</a>\
         <ul>\
-          <li><a href="publications.html?id=journal#journal">Journal</a></li>\
-          <li><a href="publications.html?id=conference#conference">Conference</a></li>\
+          <li><a href="/publications.html?id=journal#journal">Journal</a></li>\
+          <li><a href="/publications.html?id=conference#conference">Conference</a></li>\
         </ul>\
       </li>\
-      <li><a href="resources.html">Resources</a>\
+      <li><a href="/resources.html">Resources</a>\
         <ul>\
-          <li><a href="resources.html?id=documents#documents">Document</a></li>\
-          <li><a href="resources.html?id=slides#slides">Slides</a></li>\
-          <li><a href="whitepapers.html?id=whitepapers#whitepapers">White Paper</a></li>\
-          <li><a href="publications.html">Publications</a></li>\
+          <li><a href="/resources.html?id=documents#documents">Document</a></li>\
+          <li><a href="/resources.html?id=slides#slides">Slides</a></li>\
+          <li><a href="/resources.html?id=whitepapers#whitepapers">White Paper</a></li>\
+          <li><a href="/publications.html">Publications</a></li>\
         </ul>\
       </li>\
-      <li><a href="contact.html">Contact</a></li>\
+      <li><a href="/contact.html">Contact</a></li>\
     </ul>\
   <!-- end nav -->\
   </nav>\
