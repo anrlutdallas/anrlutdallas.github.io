e829d1eaa7c7db1fab8b404ee8d22470931477aa
Caleb Fung <caleb.t.fung@gmail.com>
2017-05-23 16:38:23 -0500

    fix formatting

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -6,8 +6,9 @@ document.write('\
     <!-- copyright -->\
     <div class="copyright">\
       <ul class="menu">\
-        <li>&copy; 2017 Advanced Networks Research Lab.</li>\
+        <li>&copy; 2017 Advanced Networks Research Lab. Designed by Caleb Fung.</li>\
         <li><a href="http://www.utdallas.edu/" target="_blank">The University of Texas at Dallas.</a></li>\
+        <li><a href="https://html5up.net/" target="_blank">Template by HTML5UP.</a></li>\
       </ul>\
     </div>\
 \
