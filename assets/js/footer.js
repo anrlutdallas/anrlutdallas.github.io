f874434d777a89d2ef90bbaf37bb94f82b5ebd46
Ashkan Y <yousefpour.ashkan@gmail.com>
2017-06-14 16:20:54 -0500

    Update footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -6,8 +6,7 @@ document.write('\
     <!-- copyright -->\
     <div class="copyright">\
       <ul class="menu">\
-        <li>&copy; Advanced Networks Research Lab</li>\
-        <li><a href="http://www.utdallas.edu/" target="_blank">The University of Texas at Dallas</a></li>\
+        <li>&copy; Advanced Networks Research Lab at <a href="http://www.utdallas.edu/" target="_blank">The University of Texas at Dallas</a></li>\
         <li>Developed by <a href="/people/caleb.html" target="_blank">Caleb Fung</a> and <a href="/people/ashkan.html" target="_blank">Ashkan Yousefpour</a>.</li>\
         <li><a href="https://html5up.net/" target="_blank">Template by HTML5UP</a></li>\
       </ul>\
